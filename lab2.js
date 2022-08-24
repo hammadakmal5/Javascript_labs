76e95e3eae833a29911eef6fb5215828230cf5e8
hammadakmal5 <hammadakmal5@gmail.com>
2022-08-24 21:37:35 +0500

    lab Completed

--- lab2.js
+++ lab2.js
@@ -7,6 +7,7 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+let firstItem =  arr[0];
 
 
 ////////// PROBLEM 2 //////////
@@ -15,7 +16,8 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
-
+let lastItemRemoved = arr.pop();
+console.log(arr)
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -27,6 +29,10 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
+
+for(let i = 0; i < family.length; i++){
+    console.log(family[i]);
+}
 
 
 
@@ -42,7 +48,15 @@ let evensArr = []
 */
 
 //Code Here
+for(let i = 0; i < nums.length; i++)
+{
+    if (nums[i] % 2 === 0){
+        evensArr.push(nums[i]);
+    }
 
+}
+
+console.log(evensArr);
 
 
 ////////// PROBLEM 5 //////////
@@ -56,6 +70,24 @@ var score = 74
 */
 
 //Code Here
+
+if (score >= 90){
+    console.log('A');
+}
+else if(score >= 80 && score <= 89){
+    console.log('B');
+}
+else if(score >= 70 && score <= 79){
+    console.log('C');
+}
+else if(score >= 60 && score <= 69){
+    console.log('D');
+}
+else{
+    console.log('F');
+}
+
+
 
 
 ////////// Intermediate Problems //////////
@@ -72,6 +104,7 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
+var someNum = myFavoriteNumbers[4];
 
 ////////// PROBLEM 7 //////////
 
@@ -80,6 +113,13 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
+if(myFavoriteNumbers.length < 7){
+    console.log('There are not enough elements in this Array!.');
+}
+else{
+    someNum = myFavoriteNumbers[myFavoriteNumbers.length - 1];
+}
+console.log(someNum)
 
 ////////// PROBLEM 8 //////////
 
@@ -91,6 +131,12 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Code Here
 
+for (let i = 0; i < listOfNumbers.length; i++)
+{
+    if(listOfNumbers[i] % 3 === 0){
+        console.log(`${listOfNumbers[i]} is divisible by 3.`);
+    }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -102,6 +148,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+
+for(let i = letters.length - 1; i >=0; i--)
+{
+    console.log(letters[i])
+}
 
 
 ////////// Advanced Problems //////////
@@ -124,6 +175,26 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+switch(letterGrade){
+    case 'A':
+        console.log('The student is doing excellently.');
+        break;
+    case 'B':
+        console.log('The student is doing well.');
+        break;
+    case 'C':
+        console.log('The student is doing alright.');
+        break;
+    case 'D':
+        console.log('The student is not doing very well.');
+        break;
+    case 'F':
+        console.log('The student is failing.');
+        break;
+    default:
+        console.log('Not an eligible grade.');
+}
 
 
 
@@ -152,3 +223,19 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for (let i = 1; i <= 100; i++)
+{
+    if(i % 3 === 0){
+        console.log('Dev');
+    }
+    else if(i % 5 === 0){
+        console.log('Mountain');
+    }
+    else if (i % 3 === 0 && i % 5 === 0){
+        console.log('DevMountain')
+    }
+    else{
+        console.log(i);
+    }
+}
