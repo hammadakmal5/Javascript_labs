a181b0ce91961664e4cd9b8b99639be052944571
hammadakmal5 <hammadakmal5@gmail.com>
2022-08-24 20:59:46 +0500

    Initial Commit

---
+++ fizzBuzz.js
@@ -0,0 +1,5 @@
+// Write a program that counts from 1 to 50 in fizzbuzz fashion.
+// To do so, loop from 1 to 50 (inclusive). Each time through, if the number is evenly divisible by 3, say ‘fizz’. If the number is evenly divisible by 5, say ‘buzz’. If the number is evenly divisible by both 3 and 5, say ‘fizzbuzz’.
+
+
+// Write your solution below this line:
