45b2cde6a399b6ad14f7ea0a857838550ce9d541
hammadakmal <hammadakmal5@gmail.com>
2022-08-24 19:33:24 +0500

    Task Completed

---
+++ max_of_two_no.js
@@ -0,0 +1,18 @@
+// Write a block of code that will print out the largest value.
+
+let x = 1;
+let y = 2;
+
+// Write your code below this line.
+
+if (x > y){
+    console.log(`x = ${x} is greater than y = ${y} `);
+}
+else if (x === y){
+    console.log(`Both are equal x = ${x} & y = ${y}`);
+}
+else{
+    console.log(`y = ${y} is greater than x = ${x}`);
+}
+
+// output : y = 2 is greater than x = 1 
