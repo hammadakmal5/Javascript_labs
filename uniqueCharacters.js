178bec4024c1e5fd757e785c3b407d14edb66835
hammadakmal5 <hammadakmal5@gmail.com>
2022-08-25 16:44:50 +0500

    Task Completed

---
+++ uniqueCharacters.js
@@ -0,0 +1,26 @@
+// Given a word, return true if that word contains only unique characters. Return false otherwise.
+
+// For example:
+
+// hasUniqueChars("Monday")
+//  returns true
+// hasUniqueChars("Moonday")
+//  returns false
+// Uppercase and lowercase letters should be considered separately:
+
+// hasUniqueChars("Bob")
+//  returns true
+
+
+// Code Here
+
+function hasUniqueChars(word) {
+    let uniqueChars = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
+    }
+    return uniqueChars.size === word.length
+  }
+
+
+console.log(hasUniqueChars('Monday'))
